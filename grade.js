1cebe8229cc77a8ef90cf5088239740d0bc94f25
seongho <seongho@seonghoui-MacBookPro.local>
2023-07-04 18:12:17 +0900

    update 07/04

--- grade.js
+++ grade.js
@@ -1,136 +1,171 @@
-
+const gradeItem = require('./gradeItem');
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt:'명령하세요 : '
 });
 
-    const todos =  [ 
-        {
-            'name' : '자바스크립트 공부하기', 
-            'tags' : ['programming', 'javascript'],
-            'status' : 'todo',
-            'id' : 12123123
-        },
-        {
-            'name' : ' 그림 그리기', 
-            'tags' : ['picture', 'favorite'],
-            'status' : 'doing',
-            'id' : 312323
-        }
-    ];
-    let currentStatus = {
-        'todo':0,
-        'doing':0,
-        'done':0,
-    };
-    let statusName=["todo","doing","done"];
+const todos = gradeItem.todos;
+const currentStatus = gradeItem.currentStatus;
+const statusName = gradeItem.statusName;
+
+function initCurrentStatus(){
     todos.map((value) => {
         for (const key in currentStatus){
             if (value.status == key)
                 currentStatus[key] += 1;
         }
     });
-    function printStatus(){
-        console.log(`현재상태 : todo: ${currentStatus.todo}개, doing:${currentStatus.doing}개, done:${currentStatus.done}개`);
+}
+
+function printCurrentStatus(){
+    console.log(`현재상태 : todo: ${currentStatus.todo}개, doing:${currentStatus.doing}개, done:${currentStatus.done}개`);
+}
+
+function show(argv2){
+    if(argv2 === "all")
+        printCurrentStatus();
+    else
+        showStatus(argv2);
+}
+
+function showStatus(argv2){
+    if (!statusName.includes(argv2))
+         console.log("상태를 잘못 입력하셨습니다!");
+    else{
+        let argv2Count = currentStatus[argv2];
+        let answer = `${argv2}리스트: 총 ${argv2Count}건 : `;
+        todos.forEach((value) => {
+            if(value.status === argv2){
+                answer += `' ${value.name}, ${value.id}', `;
+            }
+        });
+        answer = answer.substring(0,answer.length-2);
+        console.log(answer);
     }
-     rl.on("line", (line) => {
-        let [order1, order2, order3] = line.split("$");
-        if(order1 == "show" ){
-            if(order2 == "all")
-                printStatus();
-            else{
-                if (!statusName.includes(order2))
-                    console.log("상태를 잘못 입력하셨습니다!");
-                else{
-                let order2Count = currentStatus[order2];
-                let answer = `${order2}리스트: 총 ${order2Count}건 : `;
-                todos.map((value, index) => {
-                    if(value.status == order2){
-                        answer += `' ${value.name}, ${value.id}'`
-                        if (index != todos.length)
-                            answer += ', ';
-                    }
-                })
-                console.log(answer);
-             }
-            }
-        }
-        else if (order1 == "add"){
-            let flag = 0;
-            let randomId = parseInt(Math.random()*100000000);
-            if (todos.length == 100000000)
-                console.log("꽉 차서 add가 불가능합니다.");
-            else{
-            do
+}
+
+function add(argv2, argv3){
+    if (todos.length === 100000000)
+        console.log("꽉 차서 add가 불가능합니다.");
+    else{
+        todos.push(makeTempObject(argv2,argv3));
+        currentStatus.todo++;
+        printCurrentStatus();
+    }
+}
+
+function makeTempObject(argv2,argv3){
+    let randomId = generateId();
+    let tempArray = argv3.replace("[", "").replace("]", "").replace(/"/gi, "").split(',');
+    let tempObject={
+        name:argv2,
+        tags:tempArray,
+        status:'todo',
+        id:randomId
+    };
+    console.log(`${argv2} 1개가 추가 됐습니다. (id : ${randomId})`);
+    return tempObject;
+}
+
+function generateId(){
+    let randomId = parseInt(Math.random()*100000000);
+    let dupflag = 0;
+    do
+    {
+        for (let todo of todos){
+            if (todo.id === randomId)
             {
-                for (let todo of todos){
-                    if (todo.id == randomId)
-                    {
-                        randomId = parseInt(Math.random()*100000000);
-                        flag = 1;
-                        break;
-                    }
-                    else
-                        flag = 0;
-                }
-            }while(flag);
-                let tempArray = order3.replace("[", "").replace("]", "").replace(/"/gi, "").split(',');
-                let tempObject={
-                    'name':order2,
-                    'tags':tempArray,
-                    'status':'todo',
-                    'id':randomId
-                };
-                todos.push(tempObject);
-                currentStatus.todo++;
-                console.log(`${order2} 1개가 추가 됐습니다. (id : ${randomId})`);
-                printStatus();
+                randomId = parseInt(Math.random()*100000000);
+                dupflag = 1;
+                break;
             }
+            else
+                dupflag = 0;
         }
-        else if (order1 == "delete"){
-            if (todos.length == 0)
-                console.log("todo-list가 비어있습니다.");
-            else{
-            let flag = 1;
-            todos.map((todo,index)=>{
-                if(order2 == todo.id)
-                {
-                    console.log(`${todo.name} ${todo.status}가 목록에서 삭제됐습니다`);
-                    currentStatus[todo.status]--;
-                    todos.splice(index,1);
-                    flag = 0;
-                }
-            });
-            if (flag)
-                console.log("없는 아이디 입니다.");
-            printStatus();
-            }
+    }while(dupflag);
+    return randomId;
+}
+
+function deleteItem(argv2){
+    if (todos.length === 0)
+        console.log("todo-list가 비어있습니다.");
+    else{
+        if (doDelte(argv2))
+            console.log("없는 아이디 입니다.");
+        printCurrentStatus();
+    }
+}
+
+function doDelte(argv2){
+    let existFlag = 1;
+    todos.forEach((todo,index)=>{
+        if(argv2 === todo.id)
+        {
+            console.log(`${todo.name} ${todo.status}가 목록에서 삭제됐습니다`);
+            currentStatus[todo.status]--;
+            todos.splice(index,1);
+            existFlag = 0;
         }
-        else if (order1 == "update"){
-            if (!statusName.includes(order3))
-                console.log("상태를 잘못 입력하셨습니다!");
-            else{
-                let flag = 1;
-            todos.map((todo,index)=>{
-                if(order2 == todo.id)
-                {
-                    console.log(`${todo.name} ${order3}으로 상태가 변경됐습니다`);
-                    currentStatus[todo.status]--;
-                    todos[index].status = order3;
-                    currentStatus[todo.status]++;
-                    flag = 0;
-                }
-            });
-            if (flag)
-                console.log("없는 아이디 입니다.");
-            printStatus();
-            }
+    });
+    return existFlag;
+}
+
+function update(argv2,argv3){
+    if (!statusName.includes(argv3))
+        console.log("상태를 잘못 입력하셨습니다!");
+    else{
+        if (doUpdate(argv2,argv3))
+            console.log("없는 아이디 입니다.");
+        printCurrentStatus();
+    }
+}
+
+function doUpdate(argv2,argv3){
+    let existFlag = 1;
+    todos.forEach((todo,index)=>{
+        if(argv2 === todo.id)
+        {
+            console.log(`${todo.name} ${argv3}으로 상태가 변경됐습니다`);
+            currentStatus[todo.status]--;
+            todos[index].status = argv3;
+            currentStatus[todo.status]++;
+            existFlag = 0;
         }
+    });
+    return existFlag;
+}
+
+function clear(){
+    todos.splice(0);
+    for(const key in currentStatus)
+        currentStatus[key] = 0;
+    console.log("초기화 했습니다.");
+    printCurrentStatus();
+}
+
+function excute(){
+    rl.question("명령하세요 : ", (line) => {
+    let [argv1, argv2, argv3] = line.split("$");
+        if(argv1 === "show" )
+            show(argv2);
+        else if (argv1 === "add")
+            add(argv2,argv3);
+        else if (argv1 === "delete")
+            deleteItem(argv2);
+        else if (argv1 === "update")
+            update(argv2,argv3);
+        else if (argv1 === "clear")
+            clear();
         else
             rl.close();
+    excute();
     });
     rl.on('close', () => {
+        console.log("잘못입력하셔서 종료합니다.");
         process.exit();
-    })
+    });
+};
+
+initCurrentStatus();
+excute();
